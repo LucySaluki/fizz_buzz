19b6056e750fc8573edef48a29a48f550392d53a
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-07-23 11:50:33 +0100

    alternative solution for code

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,21 +1,23 @@
 # class FizzBuzz:
 
 def fizz_buzz(num):
-    if (num % 3 ==0 and num % 5==0):
-        return "FizzBuzz"
-    if(num % 3 == 0):
-        return "Fizz"
-    if (num % 5 ==0):
-        return "Buzz"
-    else:
-        return str(num)
-    # string=""
-    # if (num % 3 == 0):
-    #     string=string+"Fizz"
-
-    # if(num %5==0):
-    #     string=string+"Buzz"
+    # if (num % 3 ==0 and num % 5==0):
+    #     return "FizzBuzz"
+    # if(num % 3 == 0):
+    #     return "Fizz"
+    # if (num % 5 ==0):
+    #     return "Buzz"
     # else:
-    #     string=str(num)
+    #     return str(num)
+    string=""
 
-    # return string
+    if (num % 3 == 0):
+        string=string+"Fizz"
+
+    if(num %5==0):
+        string=string+"Buzz"
+
+    if (num %3 != 0 and num %5 !=0):
+        string=str(num)
+
+    return string
